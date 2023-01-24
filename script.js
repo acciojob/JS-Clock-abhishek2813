2766886d64a4f63235bdfb64c8e68fafe36fa0b7
Abhishek Pandey <98042094+abhishek2813@users.noreply.github.com>
2023-01-24 16:31:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ setInterval(() => {
 	hr = d.getHours();
 	min = d.getMinutes();
 	sec = d.getSeconds();
-	hr_rotation = 30 * hr + min / 2; //converting current time
-	min_rotation = 6 * min;
-	sec_rotation = 6 * sec;
+	
+	hr_rotation = ((hr / 12) * 360) + ((min/60)*30) + 90; 
+	min_rotation = ((min / 60) * 360) + ((sec/60)*6) + 90;
+	sec_rotation = ((sec / 60) * 360) + 90;
 
 	hour.style.transform = `rotate(${hr_rotation}deg)`;
 	minute.style.transform = `rotate(${min_rotation}deg)`;
